682651d7db33ee4c87c93f678a79dc57cad056be
hoangtoeic <mr.hoangenglish@gmail.com>
2022-01-05 15:02:20 +0700

    test

--- src/db/migrations/1640973978187-update-new-column.ts
+++ src/db/migrations/1640973978187-update-new-column.ts
@@ -1,22 +1,18 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class updateNewColumn1640973978187 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
-             
-        await queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
         ALTER TABLE "User"
-         ADD COLUMN refreshtoken VARCHAR(50) NOT NULL DEFAULT('111')`)
-         await queryRunner.query(
-       ` ALTER TABLE "User"
-         ADD COLUMN refreshtokenexpires VARCHAR(50) NOT NULL DEFAULT('222')`)
+         ADD COLUMN "refreshToken" VARCHAR(250) NOT NULL DEFAULT('111')`);
+    await queryRunner.query(
+      ` ALTER TABLE "User"
+         ADD COLUMN "refreshTokenExpired" DATE NOT NULL DEFAULT CURRENT_DATE`,
+    );
+  }
 
-
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropColumn("User", "refreshtoken");
-        await queryRunner.dropColumn("User", "refreshtokenexpires");
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('User', 'refreshToken');
+    await queryRunner.dropColumn('User', 'refreshTokenExpired');
+  }
 }
